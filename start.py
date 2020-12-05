54ac19d81398002b3cd8948a2118d53281595180
Raiishin <gavin.tanjy11@gmail.com>
2020-12-05 13:53:04 +0800

    Added in getUsersTopArtists and getUsersTopTracks

--- start.py
+++ start.py
@@ -1,25 +1,51 @@
 import requests
+import json
 from pprint import pprint
 
 clientId = ''
 clientSecret = ''
 redirectUri = ''
+
 accessToken = ''
+baseUrl = 'https://api.spotify.com/v1/'
+headers = {'Authorization': 'Bearer {token}'.format(token=accessToken)}
 
 
-def pullUserInfo(accessToken):
-
-    baseUrl = 'https://api.spotify.com/v1/'
-    headers = {'Authorization': 'Bearer {token}'.format(token=accessToken)}
-
+def getUserInfo():
+    # Get User Info
     userInfo = requests.get(baseUrl + 'me', headers=headers)
 
-    userPlaylists = requests.get(
-        baseUrl +
-        'users/{user_id}/playlists'.format(user_id=userInfo.json()['id']),
-        headers=headers)
+    # Get User's Saved Songs
+    userSavedSongs = requests.get(baseUrl + 'me/tracks?limit=5',
+                                  headers=headers).json()
 
-    userSavedSongs = requests.get(baseUrl + 'me/tracks', headers=headers)
+    # Print information on each saved song
+    for song in userSavedSongs['items']:
+        artistName = song['track']['album']['artists'][0]['name']
+        songName = song['track']['name']
+
+        print("Artist : %s, Song : %s" % (artistName, songName))
 
 
-pullUserInfo(accessToken)
+def getUsersTopArtists():
+    usersTopArtists = requests.get(baseUrl + 'me/top/artists?limit=5',
+                                   headers=headers).json()
+
+    # Print information on each top artist
+    for artist in usersTopArtists['items']:
+        artistName = artist['name']
+        print("Artist Name : %s" % artistName)
+
+
+def getUsersTopTracks():
+    usersTopTracks = requests.get(baseUrl + 'me/top/tracks?limit=5',
+                                  headers=headers).json()
+
+    # Print information on each top artist
+    for track in usersTopTracks['items']:
+        songName = track['name']
+        print("Song Name : %s" % songName)
+
+
+getUsersTopArtists()
+getUsersTopTracks()
